473b8c00d604b8bff93f303c299bf8dd0642f7aa
User <User@DESKTOP-O4VF9EC>
2023-03-16 15:23:25 +0900

    member menu

---
+++ xmas/src/xmas/MenuManager.java
@@ -0,0 +1,13 @@
+package xmas;
+
+public class MenuManager {
+
+	public static void main(String[] args) {
+		System.out.println("1. Add Members");
+		System.out.println("2. Delete Members");
+		System.out.println("3. Edit Members");
+		System.out.println("4. View Members");
+		System.out.println("5. Exit");
+	}
+
+}
