1be7329ae044f135eb78c0c45d9a79994a6318b6
User <User@DESKTOP-O4VF9EC>
2023-04-01 20:54:04 +0900

    implement each function for each menu, except menu 3 - Edit member and need to use struct

--- xmas/src/xmas/MenuManager.java
+++ xmas/src/xmas/MenuManager.java
@@ -6,6 +6,8 @@ public class MenuManager {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
+		String MemberName = new String();
+		int NumAccept = 0;
 		
 		while(true) {
 			System.out.println("1. Add Member");
@@ -16,16 +18,39 @@ public class MenuManager {
 			System.out.println("6. Exit");
 			String a = input.next();
 			char str = a.charAt(0);
-			if(str == '5') {
+			if(str == '1') {
+				System.out.println("메뉴로 나가시려면 0을, 회원 입력을 계속하시려면 1을 입력하십시오: ");
+				NumAccept = input.nextInt();				
+				if(NumAccept == 0) continue;
+				else if(NumAccept == 1) {
+					MemberName = input.next();
+					continue;
+				}
+			}
+			else if(str == '2') {
+				System.out.println("회원 정보를 삭제하시려면 1을, 메뉴로 나가시려면 0을 입력하세요:");
+				NumAccept = input.nextInt();
+				if(NumAccept == 0) continue;
+				else if(NumAccept == 1) {
+					MemberName = "empty";
+					System.out.println(MemberName);
+					continue;
+				}
+			}
+			else if(str == '3') {
+				//추후 구현 필요 , 구조체를 이용하여 회원 정보를 저장하고 묶어서 보여줌
+			}
+			else if(str == '4') {
+				System.out.println(MemberName);
+			}
+			else if(str == '5') {
 				continue;
 			}
-			
-			else if(str == '6') {
-				System.out.println("Thank you for Subscribing!");
-				break;
-			}
-			else break;
+			else if(str == '6') break;
 		}
+		
+		
+		
 	}
 
 }
